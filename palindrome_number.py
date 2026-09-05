bdb627ba44528afa85474d605bd2149a9a8c6d82
Nandana <vvce24cseaiml0088@vvce.ac.in>
2026-09-05 21:37:59 +0530

    palindrome number

---
+++ palindrome_number.py
@@ -0,0 +1,16 @@
+def isPalindrome(x):
+    if x <0:
+        return False
+
+    temp=x
+    rev=0
+    while x>0:
+        rem=x%10
+        rev=rev*10+rem
+        x=x//10
+
+    return rev==temp
+
+x=int(input("Enter a number: "))
+print(isPalindrome(x))
+        
